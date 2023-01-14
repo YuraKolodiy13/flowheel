dee3ed834ac3c7c421a4bb55fa2d74da90f50be1
YuraKolodiy13 <timber2017@gmail.com>
2023-01-14 17:20:35 +0100

    add responsive version

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
 import './Header.scss';
 import Nav from "../Nav/Nav";
 
 const Header = () => {
+
+  const [scroll, setScroll] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => setScroll(window.scrollY > 0));
+  }, []);
+
   return (
-    <header className='header'>
+    <header className={`header ${scroll ? 'fixed' : ''}`}>
       <div className="header__container">
         <h3 className='header__logo'>
           <Link to='/'>flo.wheel</Link>
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from "../Button/Button";
 import Menu from '../Menu/Menu';
 import './Nav.scss';
@@ -50,28 +50,36 @@ const menuItems = [
   },
 ];
 
-const Nav = () => (
-  <nav className='nav'>
-    <div className="nav__left">
-      <ul className="nav__menus">
-        {menuItems.map((menu: IMenuItem, index: number) =>
-          <Menu items={menu} key={index} />
-        )}
-      </ul>
-    </div>
-    <div className="nav__user">
-      <Button
-        title='Login'
-        color='inverse'
-        doAction={() => {}}
-      />
-      <Button
-        title='Sign Up'
-        color='primary'
-        doAction={() => {}}
-      />
-    </div>
-  </nav>
-)
+const Nav = () => {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+  return (
+    <nav className='nav'>
+      <div className={`nav__toggle ${openMenu ? 'close' : ''}`} onClick={() => setOpenMenu(!openMenu)}>
+        <span/>
+      </div>
+      <div className={`nav__container ${openMenu ? 'open' : ''}`}>
+        <div className="nav__left">
+          <ul className="nav__menus">
+            {menuItems.map((menu: IMenuItem, index: number) =>
+              <Menu items={menu} key={index} />
+            )}
+          </ul>
+        </div>
+        <div className="nav__user">
+          <Button
+            title='Login'
+            color='inverse'
+            doAction={() => {}}
+          />
+          <Button
+            title='Sign Up'
+            color='primary'
+            doAction={() => {}}
+          />
+        </div>
+      </div>
+    </nav>
+  )
 
+}
 export default Nav;
